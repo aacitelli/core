c2fdd4ae18316636d89f077f9ab0d6d779bed895
Anden Acitelli <a.acitelli@gmail.com>
2021-02-09 23:25:51 -0500

    Function outlines

---
+++ tokenizer.py
@@ -0,0 +1,24 @@
+def main():
+    pass
+
+# Returns (info about) current token; 
+# Repeated calls to getToken() return the same token 
+def getToken(): 
+    pass
+
+# Skips current token; next getToken() call will return new token 
+def skipToken():
+    pass 
+
+# Returns the value of the current (integer) token 
+# Errors out if the current token is not an integer 
+def intVal():
+    pass
+
+# Returns the name (string) of the current (id) token 
+# Errors out if the current token is not an id 
+def idName(): 
+    pass 
+
+if __name__ == "__main__":
+    main()
