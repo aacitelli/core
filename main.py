3048b944ee545a0e8585f75955ecfcc94f9aca85
Anden Acitelli <a.acitelli@gmail.com>
2021-02-12 02:00:56 -0500

    Working for the basic test case!

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import sys 
+import sys
 from tokenizer import Tokenizer
+
 
 def main(argv):
 
-    # Open file and handle any errors 
-    file = open(argv[0], "r")
+    # Open file and handle any errors
+    f = open(argv[0], "r")
 
-    # Initialize tokenizer and pass file object 
-    Tokenizer(file)
+    # Initialize tokenizer and pass file object
+    tokenizer = Tokenizer(f)
+
+    # Loop and print result until we get to it returning an EOF character
+    #print("calling getToken()")
+    token = tokenizer.getToken()
+    while token != 33:
+        print("{}".format(token), end=" ")
+        #print("calling skipToken()")
+        tokenizer.skipToken()
+        #print("calling getToken()")
+        token = tokenizer.getToken()
+    print("33") # We always exit the loop on an eof; probably a cleaner way, but this works 
 
     # Close file
-    file.close()
-    
+    f.close()
+
 if __name__ == "__main__":
     main(sys.argv[1:])
--- tokenizer.py
+++ tokenizer.py
@@ -1,24 +1,141 @@
-class Tokenizer: 
+class Tokenizer:
 
-    def __init__(self, file):
-        self.tokens = []
-        self.file = file
+    tokenToID = {
+        "program": 1,
+        "begin": 2,
+        "end": 3,
+        "int": 4,
+        "if": 5,
+        "then": 6,
+        "else": 7,
+        "while": 8,
+        "loop": 9,
+        "read": 10,
+        "write": 11,
+        ";": 12,
+        ",": 13,
+        "=": 14,
+        "!": 15,
+        "[": 16,
+        "]": 17,
+        "&&": 18,
+        "||": 19,
+        "(": 20,
+        ")": 21,
+        "+": 22,
+        "-": 23,
+        "*": 24,
+        "!=": 25,
+        "==": 26,
+        "<": 27,
+        ">": 28,
+        "<=": 29,
+        ">=": 30
+    }
 
-    # Returns (info about) current token; 
-    # Repeated calls to getToken() return the same token 
-    def getToken(self): 
-        pass
+    idToToken = {
+        1: "program",
+        2: "begin",
+        3: "end",
+        4: "int",
+        5: "if",
+        6: "then",
+        7: "else",
+        8: "while",
+        9: "loop",
+        10: "read",
+        11: "write",
+        12: ";",
+        13: ",",
+        14: "=",
+        15: "!",
+        16: "[",
+        17: "]",
+        18: "&&",
+        19: "||",
+        20: "(",
+        21: ")",
+        22: "+",
+        23: "-",
+        24: "*",
+        25: "!=",
+        26: "==",
+        27: "<",
+        28: ">",
+        29: "<=",
+        30: ">="
+        # 31 = Integer
+        # 32 = Identifier
+        # 33 = EOF
+    }
 
-    # Skips current token; next getToken() call will return new token 
+    def __init__(self, f):
+        self.f = f
+        self.line = f.readline().strip()
+
+    # Returns (info about) current token;
+    # Repeated calls to getToken() return the same token
+    # Precondition: The first character of self.line() is the beginning of our character and is not whitespace
+    def getToken(self):
+
+        # If we start out not immediately on a token (due to stuff skipToken sets), we know we're at EOF
+        if len(self.line) == 0:
+            # print()
+            return 33
+
+        # Loop until we get (1) End of line or (2) White space or (3) Newline character
+        token = ""
+        pos = 0
+        while pos < len(self.line) and self.line[pos] != " " and self.line[pos] != "\n":
+            token += self.line[pos]
+            pos += 1
+            #print("token {} from pos {}".format(token, pos))
+
+        # If it's one of our vanilla special characters
+        if token in self.tokenToID:
+            return self.tokenToID[token]
+
+        # Debug 
+        # print("Special Character {}".format(token))
+
+        # Check if it's an integer
+        if token.isdigit():
+            return 31
+        return 32 # Identifier      
+
+    # Skips current token; next getToken() call will return new token
+    # AKA moves our input such that the current beginning of the line is the next character
     def skipToken(self):
-        pass 
 
-    # Returns the value of the current (integer) token 
-    # Errors out if the current token is not an integer 
-    def intVal(self):
+        # print("Line pre-skipToken(): {}".format(self.line))
+
+        # Strip our line to be sure
+        self.line = self.line.strip()
+
+        # If we're at the very end of a line, read in a new line, strip it, and return 
+        if not len(self.line):
+            self.line = self.f.readline()
+            return
+
+        # Keep trimming stuff off until we either:
+        # (1) Get to a newline, meaning we should read in a new line, then strip it, then return 
+        # (2) Get to a space, meaning we should strip our current line then return 
+        while len(self.line) and self.line[0] != "\n" and self.line[0] != " ":
+            self.line = self.line[1:]
+            # print("self.line: {}".format(self.line))
+        if not len(self.line) or self.line[0] == "\n":
+            self.line = self.f.readline().strip()
+        else:
+            self.line = self.line.strip()
+
+        # print("Line post-skipToken(): {}".format(self.line))
+
+    # Returns the value of the current (integer) token
+    # Errors out if the current token is not an integer
+    def intVal(self, str):
         pass
 
-    # Returns the name (string) of the current (id) token 
-    # Errors out if the current token is not an id 
-    def idName(self): 
+    # Returns the name (string) of the current (id) token
+    # Errors out if the current token is not an id
+    def idName(self, str):
         pass
