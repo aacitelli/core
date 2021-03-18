e237a95ed585e184974c112219a5f40a9f325c98
Anden Acitelli <a.acitelli@gmail.com>
2021-03-18 14:43:38 -0400

    Fix IF issue

--- parser/If.py
+++ parser/If.py
@@ -1,70 +1,74 @@
-import t 
-import Cond 
+import t
+import Cond
 import StmtSeq
 
-class If():
 
+class If():
     def __init__(self):
         self.__c = None
         self.__ss1 = None
         self.__ss2 = None
-        self.__alternative = None 
+        self.__alternative = None
 
-    def parse(self): 
+    def parse(self):
 
-        # `if` token 
+        # `if` token
         tokNo = t.tokenizer.get_token()
         t.tokenizer.skip_token()
-        if tokNo != t.tokenizer.IF.value:
-            print("If: Expected token {}, got token {}".format(t.tokenizer.IF.value, tokNo))
-            return -1 
+        if tokNo != t.Tokens.IF.value:
+            print("If: Expected token {}, got token {}".format(
+                t.tokenizer.IF.value, tokNo))
+            return -1
 
         # Parse Condition
         self.__c = Cond.Cond()
         self.__c.parse()
 
-        # `then` token 
+        # `then` token
         tokNo = t.tokenizer.get_token()
         t.tokenizer.skip_token()
-        if tokNo != t.tokenizer.THEN.value:
-            print("If: Expected token {}, got token {}".format(t.tokenizer.THEN.value, tokNo))
-            return -1 
+        if tokNo != t.Tokens.THEN.value:
+            print("If: Expected token {}, got token {}".format(
+                t.tokenizer.THEN.value, tokNo))
+            return -1
 
-        # `stmtseq` token 
+        # `stmtseq` token
         self.__ss1 = StmtSeq.StmtSeq()
         self.__ss1.parse()
 
-        # If we're at an "end", it means there's no `else` and that we should get out 
+        # If we're at an "end", it means there's no `else` and that we should get out
         tokNo = t.tokenizer.get_token()
         t.tokenizer.skip_token()
-        if tokNo == t.tokenizer.END.value:
+        if tokNo == t.Tokens.END.value:
             self.__alternative = 1
             return 0
 
-        # Otherwise, we're at an else 
-        if tokNo != t.tokenizer.ELSE.value:
-            print("If: Expected token {}, got token {}".format(t.tokenizer.ELSE.value, tokNo))
+        # Otherwise, we're at an else
+        if tokNo != t.Tokens.ELSE.value:
+            print("If: Expected token {}, got token {}".format(
+                t.tokenizer.ELSE.value, tokNo))
             return -1
 
-        # `stmtseq` token 
+        # `stmtseq` token
         self.__alternative = 2
         self.__ss2 = StmtSeq.StmtSeq()
         self.__ss2.parse()
 
-        # `end` token 
+        # `end` token
         tokNo = t.tokenizer.get_token()
         t.tokenizer.skip_token()
-        if tokNo != t.tokenizer.END.value:
-            print("If: Expected token {}, got token {}".format(t.tokenizer.END.value, tokNo))
-            return -1 
+        if tokNo != t.Tokens.END.value:
+            print("If: Expected token {}, got token {}".format(
+                t.tokenizer.END.value, tokNo))
+            return -1
 
     def exec(self):
         if self.__c.is_true:
-            return 
+            return
         if self.__alternative == 1:
             self.__ss1.exec()
         else:
-            self.__ss2.exec()        
+            self.__ss2.exec()
 
     def print(self, indentation):
         print(" " * indentation, end="")
